525133ec9d746c318a0c73e79dabe2db06693f3c
van-yog <ikharchenko2008@gmail.com>
2021-01-18 13:45:46 +0300

    add modal, REST API, Loader

--- src/App.js
+++ src/App.js
@@ -2,19 +2,36 @@ import React from "react";
 import "./App.css";
 import TodoList from "./Components/TodoList";
 import Context from "./context";
+import { useEffect } from "react";
+import Loader from "./Components/Loader.js";
+import Modal from "./Components/Modal/Modal.js";
+import AddTodo from "./Components/AddTodo.js";
+
+// Ленивая загрузка компонента AddTodo
+// const AddTodo = React.lazy(
+//   () =>
+//     new Promise((resolve) => {
+//       setTimeout(() => {
+//         resolve(import("./Components/AddTodo.js"));
+//       }, 2000);
+//     })
+// );
 
 function App() {
-  const [todos, setTodos] = React.useState([
-    { id: 1, completed: false, title: "Купить хлеб" },
-    { id: 2, completed: true, title: "Купить масло" },
-    { id: 3, completed: false, title: "Купить батон" },
-  ]);
+  const [todos, setTodos] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
 
-  // let todos = [
-  //   { id: 1, completed: false, title: "Купить хлеб" },
-  //   { id: 2, completed: false, title: "Купить масло" },
-  //   { id: 3, completed: false, title: "Купить батон" },
-  // ];
+  // useEffect( callback, []) - эмуляция componentDidMount
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then((response) => response.json())
+      .then((todos) => {
+        setTimeout(() => {
+          setTodos(todos);
+          setLoading(false);
+        }, 1000);
+      });
+  }, []);
 
   function toggleTodo(id) {
     setTodos(
@@ -31,11 +48,30 @@ function App() {
     setTodos(todos.filter((todo) => todo.id !== id));
   }
 
+  function addTodo(title) {
+    setTodos(todos.concat([{ title, id: Date.now(), completed: false }]));
+  }
+
   return (
     <Context.Provider value={{ removeTodo: removeTodo }}>
       <div className="wrapper">
         <h2>React tutorial</h2>
-        <TodoList todos={todos} onToggle={toggleTodo} />
+        <Modal />
+
+        {/* // Ленивая загрузка компонента AddTodo */}
+        {/* <React.Suspense fallback={<p>Ждите плиз...</p>}>
+          <AddTodo onCreate={addTodo} />
+        </React.Suspense> */}
+
+        <AddTodo onCreate={addTodo} />
+
+        {loading && <Loader />}
+
+        {todos.length ? (
+          <TodoList todos={todos} onToggle={toggleTodo} />
+        ) : loading ? null : (
+          <p>No todos!</p>
+        )}
       </div>
     </Context.Provider>
   );
